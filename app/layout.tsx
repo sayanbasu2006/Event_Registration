3d56435d0a1b84654d241767582c545a018579be
Sayan Basu <sayanbasu2006@gmail.com>
2025-10-12 14:32:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Even Registration App",
+  description: "Created with VSCode,
+  generator: "Terminal",
 }
 
 export default function RootLayout({
